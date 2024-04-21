9fbf29d793b3dd210a3dccec42b6f6fa4151fdd7
VinczePistukaHunor <vhunor2002@yahoo.com>
2024-04-22 00:21:22 +0300

    Lab Session 4

---
+++ Lab_5/src/App4_Lab7/Main.java
@@ -0,0 +1,17 @@
+
+package App4_Lab7;
+import java.lang.invoke.SwitchPoint;
+import java.util.concurrent.Semaphore;
+public class Main {
+    public static void main(String[] args) {
+
+        Semaphore sem = new Semaphore(2);
+
+        int[] t1 = {3, 6, 5};
+        int[] t2 = {4, 7, 3};
+        int[] t3 = {5, 7, 6};
+        new ExecutionThread(sem, t1).start();
+        new ExecutionThread(sem, t2).start();
+        new ExecutionThread(sem, t3).start();
+    }
+}
